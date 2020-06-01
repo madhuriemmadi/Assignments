19b967d90e96a2e25475f531aba4fd49788a5cf2
Madhuri emmadi <madhuri.emmadi8@gmail.com>
2020-06-01 11:42:01 -0400

    default method in interface

--- NumberDemo/src/main/java/com/example/CarTest/VehicleInventory.java
+++ NumberDemo/src/main/java/com/example/CarTest/VehicleInventory.java
@@ -5,5 +5,7 @@ public interface VehicleInventory {
 	public void addCar(BaseCar bc);
 	public void removeCar(BaseCar bc);
 	public void updateInventory(String part,Integer count);
-	
+	default void getinventoryDetails() {
+		System.out.print("This inventory has detilas about cars  and the parts available in the inventory");
+	}
 }
