6c923eb7e5b90fd401cbe4905c51e7d0e084aa6a
Madhuri emmadi <madhuri.emmadi8@gmail.com>
2020-06-01 11:40:59 -0400

    Added default method

--- NumberDemo/src/main/java/com/example/CarTest/CarMain.java
+++ NumberDemo/src/main/java/com/example/CarTest/CarMain.java
@@ -6,13 +6,15 @@ public class CarMain {
 		
 		ToyotaCar hc1 = new HondaCar("civic", 2020, 32000.0, "grey");
 		VehicleInventoryImpl vi=new VehicleInventoryImpl();
+		//calling defualt method of interface
+		vi.getinventoryDetails();
 		vi.addCar(new HondaCar("civic", 2019, 25000.0, "red"));
 		vi.addCar(new ToyotaCar("Camry", 2017, 22000.0, "black"));
 		vi.addCar(hc1);
 		vi.updateInventory("wheels", 100);
 		vi.updateInventory("breaks", 50);
 		vi.removeCar(hc1);
-		
+		vi.getinventoryDetails();
 		double rsv=hc1.calcResaleValue(hc1.getPrice());
 		System.out.print("Resale value is :"+rsv);
 	}
